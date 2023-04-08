7a91a5d9ccd29fa6a45de1f8f935c0b04083fd6b
Aishen <aish3n@pm.me>
2023-04-09 07:03:55 +0800

    chore(gui): disable pygame prompt

--- gui/src/__main__.py
+++ gui/src/__main__.py
@@ -1,12 +1,17 @@
-import pygame
+from os import environ
+environ["PYGAME_HIDE_SUPPORT_PROMPT"] = "1"
+
+from pygame.font import init as pyg_font_init
+from pygame import init as pyg_init
+
 from .app import App
 
 
 def main():
     app = App()
 
-    pygame.init()
-    pygame.font.init()
+    pyg_init()
+    pyg_font_init()
 
     app.init()
 
